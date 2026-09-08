609cf4d52c7d574c80abeada4b9aab8a26de3ea8
Ale <erickmagar332@gmail.com>
2026-09-08 12:20:11 -0600

    Eliminando funión serie factorial

--- factorialFor.js
+++ factorialFor.js
@@ -5,10 +5,6 @@ function factorial(numero) {
 
     let resultado = 1;
 
-    for (let contador = 2; contador <= numero; contador++) {
-        resultado *= contador;
-    }
-
     return resultado;
 }
 
